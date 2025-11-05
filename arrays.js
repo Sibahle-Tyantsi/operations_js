f7706e955687f998a6e97fecce483414c7815788
Siya021 <siyamthandamazokwan@gmail.com>
2025-11-05 02:15:32 +0200

    Add arrays file

---
+++ arrays.js
@@ -0,0 +1,17 @@
+
+let colors = [];
+
+colors.push("blue");
+colors.push("purple");
+colors.push("black");
+
+let numbers = [1,2,3,4.5]
+
+colors.pop()
+
+colors.unshift("pink")
+
+console.log("Colors array:", colors);
+console.log("Numbers array:", numbers);
+console.log("Length of colors array:", colors.length);
+console.log("Length of numbers array:", numbers.length);
